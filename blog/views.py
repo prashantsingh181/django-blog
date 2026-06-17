08c6a7ce3c0299abfe98fc8079d6e51faa326a82
Prashant Singh <prashant.singh@shine.com>
2026-06-17 19:32:27 +0530

    make post model and query post

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        "title": "Post 1",
-        "content": "Content of post 1",
-        "author": "Prashant Singh",
-        "date_posted": "August, 26",
-    },
-    {
-        "title": "Post 2",
-        "content": "Content of post 2",
-        "author": "Nishita Singh",
-        "date_posted": "May, 26",
-    },
-    {
-        "title": "Post 3",
-        "content": "Content of post 3",
-        "author": "S B Singh",
-        "date_posted": "January, 26",
-    },
-]
-
+from .models import Post
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
 def about(request):
     context = {"title": "About"}
     return render(request, "blog/about.html", context)
+    
