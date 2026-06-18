5b1ef90f0a91ebb90b6a54627fd894427a4d5ee7
Prashant Singh <prashant.singh@shine.com>
2026-06-18 15:37:41 +0530

    login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
@@ -16,3 +17,8 @@ def register(request):
         form = UserRegistrationForm()
 
     return render(request, "users/registration.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
