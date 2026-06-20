40271ddddb08bcd958abca62bf353619c9081681
Prashant Singh <prashant.singh181@gmail.com>
2026-06-20 11:45:56 +0530

    post create update delete view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 app_name = "blog"
 
-urlpatterns = [path("", views.home, name="home"), path("about/", views.about, name="about")]
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="home"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("post/new/", views.PostCreateView.as_view(), name="create"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
+    path("about/", views.about, name="about"),
+]
